831ab37260c7273783977f6dd10c069c2e5d6894
Anand J <anand.indukala@gmail.com>
2015-01-11 02:07:45 +0530

    Added UserDetailsManager

--- login/models.py
+++ login/models.py
@@ -8,9 +8,23 @@ class Address(models.Model):
 	state = models.CharField(max_length=50,default='asfdf')
 	pin = models.CharField(max_length=10,default='asfdf')
 	landmark = models.CharField(max_length=200,default='asfdf')
+	phone = models.CharField(max_length=11,default='asfdf')
+
+class UserDetailsManager(models.Manager):
+	def create_user_details(self,username,password,email,
+		addr1=None,addr2=None,city=None,state=None,pin=None,landmark=None,phone=None):
+		user = User.objects.create_user(username,email,password)
+		# if addr1 is None or addr2 is None or city is None or state is None or pin is None or phone is None:
+		addr = Address()
+		addr.save()
+		
+		ud = UserDetails(user=user,address=addr)
+		ud.save()
+		return ud
+
 
 class UserDetails(models.Model):
 	user = models.OneToOneField(User)
 	address = models.ForeignKey(Address)
-	phone = models.CharField(max_length=11,default='asfdf')
 
+	objects = UserDetailsManager()
