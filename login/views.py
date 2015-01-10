3230dc371001ccfac56f41c5faeed4f190bdab66
Anand J <anand.indukala@gmail.com>
2015-01-11 02:13:20 +0530

    Added register view-controller

--- login/views.py
+++ login/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth import logout, login, authenticate
+from django.db import IntegrityError
+
+from login.models import UserDetails
 
 import logging
 
 # Getting logger instance
 login_logger = logging.getLogger(__name__)
 
+#
+# Login logic
+#
 def index(request):
 	if request.user.is_authenticated():
 		return redirect(reverse('home:index'), permenant=True)
@@ -34,3 +40,33 @@ def auth(request):
 def outlog(request):
 	logout(request)
 	return redirect(reverse('login:index'), permenant=True)
+
+#
+# Registration logic
+#
+def register(request):
+	context = {}
+	if request.POST.get('submit') is not None:
+		username = request.POST['username']
+		email = request.POST['email']
+		password = request.POST['password']
+		confirm = request.POST['confirm']
+		try:
+			if password == confirm:
+				ud = UserDetails.objects.create_user_details(username,email,password)
+				context = {
+					"success_message":"Successfully Registered",
+					}
+			else:
+				context = {
+					"error_message":"Password mismatch",
+					}
+		except IntegrityError:
+			login_logger.error("username exist")
+			context = {
+				"error_message":"Username already exist",
+				}
+
+	return render(request, 'login/register.html', context)
+
+
