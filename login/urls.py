de39b9878f1160bb7c5b3a6bd6265babf1d0ace6
Anand J <anand.indukala@gmail.com>
2015-01-11 02:09:39 +0530

    Added url for register

--- login/urls.py
+++ login/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^auth$', views.auth, name='auth'),
     url(r'^logout$', views.outlog, name='logout'),
+    url(r'^register$', views.register, name='register'),
 )
