915d157828333cec968eff7daff59591f7dc877e
Anand J <anand.indukala@gmail.com>
2015-01-13 01:23:43 +0530

    Books model code initial commit

--- books/models.py
+++ books/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+	name = models.CharField(max_length=50)
+	author = models.ForeignKey(Author)
+
+class Author(models.Model):
+	name = models.CharField(max_length=50)
+	desc = models.CharField(max_length=1000)
+	born = models.DateField()
+	died = models.DateField()
+	gender = models.CharField(max_length=1)
+	website = models.CharField(max_length=100)
+
+class Book(models.Model):
+	name = models.CharField(max_length=100)
+	desc = models.CharField(max_length=1000)
+	author = models.ManyToManyField(Author)
+	genre = models.ForeignKey(Genre)
+	pages = models.IntegerField()
+	edition = models.IntegerField()
+	isbn = models.CharField(max_length=50)
