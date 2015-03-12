73c9a0f45f6f649f7d1e2a121a938cf6f96f1b01
Anand J <anand.indukala@gmail.com>
2015-03-13 00:14:57 +0530

    Added buy and sell

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
 
-from books import views
+from shop import views
 
 urlpatterns = patterns('',
 		url(r'^$', views.index, name='index'),
+		url(r'^buy$', views.buy, name='buy'),
 )
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from books.models import Book, BookAuthor, Author, Genre, AuthorGenre, BookGenre, Comments
+from shop.models import BookDetails, BuyInterests
+from login.models import UserDetails
+
+import logging
+
+
+'''
+Getting logger instance 
+'''
+shop_logger = logging.getLogger(__name__)
+
+
+'''
+Want to buy request
+'''
+def buy(request):
+	book = BookDetails.objects.get(pk=request.GET.get('id'))
+	# user = UserDetails.objects.get(user=request.user)
+	user = UserDetails.objects.get(user__username='admin')
+
+	response = True
+
+	'''
+	Register Buy Interest
+	'''
+	interest = BuyInterests.objects.create(user=user,book=book)
+
+	'''
+	Validation
+	'''
+	if interest is None:
+		response = False
+
+	'''
+	TODO: Notify the seller through internal notification and e-mail
+	'''
+
+	return JsonResponse(response, safe=False)
+
+
+'''
+Want to sell request
+'''
+def sell(request):
+	book = Book.objects.get(request.GET.get('id'))
+	owner = request.user
+	price = request.GET.get('price')
+	condition = request.GET.get('condition')
+
+	response = True
+
+	'''
+	Register a new BookDetails entry
+	'''
+	entry = BookDetails.objects.create(book=book,owner=owner,price=price,condition=condition)
+
+	'''
+	Validation
+	'''
+	if entry is None:
+		response = False
+
+	return JsonResponse(response, safe=False)
